96762bdcc3f43320a25fd9988d98acd8e95afb47
TemulenPinecone <pineconetemulen@gmail.com>
2023-12-12 15:29:15 +0800

    1212 real last

--- 5-nestedLoop/exampleFindCharacter.js
+++ 5-nestedLoop/exampleFindCharacter.js
@@ -1,4 +1,4 @@
-        // COUNT BY A AND WORD
+// COUNT BY A AND WORD
 let sentence = `Lorem, ipsum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, 
 adfadf`;
 let countA = 0;
@@ -14,35 +14,30 @@ for (let i = 0; i < sentence.length; i++) {
 console.log(`\nA character : `, countA);
 console.log(`\nWord count :`, countWord);
 
+// COUNT BY WORD and CREATE THAT WORD IN ONE INDEX
 
-
-        // COUNT BY WORD and CREATE THAT WORD IN ONE INDEX
-        
-let sentence = `Lorem, ipsum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, 
+let sentence2 = `Lorem, ipsum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, 
 adfadf`;
 let word = [];
-let count =0;
+let count = 0;
 for (let i = 0; i < sentence.length; i++) {
   if (sentence[i] == ` `) {
-        word[count] = sentence[i];
-        count++;
-    }
+    word[count] = sentence[i];
+    count++;
   }
+}
 
 // s="bat dorj"
 // if(s[i]=="") {
 // array[count] = str;
 // str="";
-// count++; 
+// count++;
 // }
 // else {
 //   str=str+s[i];
 // }
 
-
 console.log(`Word `, word);
 
-
-1,1-r ni haij ywj baigaad zai garch irwel umnuh index-uudaa hamgiin ehnii index deer nemeed 
-hoyr dahi index-s zai baigaa index-g hurtel utgiig ni hooson bolgono.
-
+// 1,1-r ni haij ywj baigaad zai garch irwel umnuh index-uudaa hamgiin ehnii index deer nemeed
+// hoyr dahi index-s zai baigaa index-g hurtel utgiig ni hooson bolgono.
