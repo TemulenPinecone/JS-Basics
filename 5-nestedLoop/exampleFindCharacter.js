2dfd59c894aba20d87e28444e882a5e406fba4c7
TemulenPinecone <pineconetemulen@gmail.com>
2023-12-12 12:37:51 +0800

    1212 lesson

---
+++ 5-nestedLoop/exampleFindCharacter.js
@@ -0,0 +1,37 @@
+        // COUNT BY A AND WORD
+let sentence = `Lorem, ipsum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, 
+adfadf`;
+let countA = 0;
+let countWord = 1;
+for (let i = 0; i < sentence.length; i++) {
+  if (sentence[i] == `a` || sentence[i] == `A`) {
+    countA++;
+  }
+  if (sentence[i] == ` `) {
+    countWord++;
+  }
+}
+console.log(`\nA character : `, countA);
+console.log(`\nWord count :`, countWord);
+
+
+
+        // COUNT BY WORD and CREATE THAT WORD IN ONE INDEX
+        
+let sentence = `Lorem, ipsum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, 
+adfadf`;
+let word = [];
+let count =0;
+for (let i = 0; i < sentence.length; i++) {
+  if (sentence[i] == ` `) {
+        word[count] = sentence[i];
+        count++;
+    }
+  }
+
+console.log(`Word `, word);
+
+
+1,1-r ni haij ywj baigaad zai garch irwel umnuh index-uudaa hamgiin ehnii index deer nemeed 
+hoyr dahi index-s zai baigaa index-g hurtel utgiig ni hooson bolgono.
+
---
+++ 5-nestedLoop/squareAsterisc.js
@@ -0,0 +1,12 @@
+function sqrStr(size) {
+  let sqrStr = "";
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      sqrStr += "*";
+    }
+    sqrStr += "\n";
+  }
+  console.log(sqrStr);
+}
+
+sqrStr(5);
