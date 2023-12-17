b641539e919dcdb6ee809dddc4c715c107139142
temulenpinecone <temulenpinecone@gmail.com>
2023-12-17 22:48:10 +0800

    12.17 home - Andy 3 doing

--- 5-nestedLoop/ex10-andyTeacher5Problems.js
+++ 5-nestedLoop/ex10-andyTeacher5Problems.js
@@ -13,18 +13,18 @@
 
 // 2.  Write a JavaScript program that accepts a number as input and inserts dashes (-) between each even number. For example if you accept 025468 the output should be 0-254-6-8.
 
-function ifEvenInsSym(inputStr) {
-  let result = ``;
-  for (i = 0; i < inputStr.length; i++) {
-    result += inputStr[i];
-    if (Number(inputStr[i]) % 2 == 0 && Number(inputStr[i + 1] % 2 == 0)) {
-      result += `-`;
-    }
-  }
-  return result;
-}
+// function ifEvenInsSym(inputStr) {
+//   let result = ``;
+//   for (i = 0; i < inputStr.length; i++) {
+//     result += inputStr[i];
+//     if (Number(inputStr[i]) % 2 == 0 && Number(inputStr[i + 1] % 2 == 0)) {
+//       result += `-`;
+//     }
+//   }
+//   return result;
+// }
 
-console.log(ifEvenInsSym(`025468`));
+// console.log(ifEvenInsSym(`025468`));
 
 // #3 : Write a JavaScript program to find the most frequent item in an array.
 // Sample array : var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
@@ -32,18 +32,24 @@ console.log(ifEvenInsSym(`025468`));
 
 var arr = [3, "a", "a", "a", 2, 3, "a", 3, "a", 2, 4, 9, 3];
 
-// arr.sort((a, b) => a - b);
-// console.log(arr);
-console.log(`arr[0]`, arr[0]);
-console.log(`arr[1]`, arr[1]);
-let count = 0,
-  char;
-for (let i = 0; i < arr.length; i++) {
-  for (let j=1; j>arr.length; j++) {
-  if (arr[i] === arr[j]) {
-    count++;
-    char = arr[i];
-    console.log(char);
+function frequentItemInArray(inputArray) {
+let max = 1;
+let result = 0, temp; 
+for (let i = 0; i < inputArray.length; i++) {
+  console.log(`i loop : `, i, `temp : `, temp);
+  for (let j=1; j<inputArray.length; j++) {
+    console.log(`     j loop :`, j );
+  if (inputArray[i] === inputArray[j]) {
+    max++;
+    temp=+inputArray[i]; console.log(`                          1st if : ` + temp + `   ( ` + max +` times )`);
+    }
+    if (max>result) {
+      result=max; console.log(`                             2nd if  frequent result number : `, result);
+    } 
   }
 }
+console.log(`result mean frequent item : `, result, `max mean times: `, max);
+return;
 }
+
+console.log(`function : `,frequentItemInArray(arr));
