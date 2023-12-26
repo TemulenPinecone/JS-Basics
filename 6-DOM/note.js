b5b44ce03da0837a3e2f6aaf95c7019256769eee
TemulenPinecone <pineconetemulen@gmail.com>
2023-12-26 21:41:08 +0800

    doing edit tag LAST

--- 6-DOM/note.js
+++ 6-DOM/note.js
@@ -9,3 +9,17 @@ const boxArray = [
 boxArray.forEach(el) => {
     boxString += `<div ondragstart="drag(event)" class="box" dragabble`
 }
+
+
+// Todo - 
+// 1. Delete zuw ustgadag bolgoh
+// 2. Edit button hiih
+// 3. Check box - Done uruu shiljih
+// 4. Drag drop
+// 5. Add card - modal
+
+
+// Calc 
+// 1. Operations
+
+// Colorful
