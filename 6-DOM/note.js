c99695b9bbf85631596fe3013411a5b65ad6135d
Тэмүлэн Галсанпунцаг <23LP2607@macbooks-MacBook-Pro.local>
2023-12-26 21:26:46 +0800

    i9 todo doing edit

---
+++ 6-DOM/note.js
@@ -0,0 +1,11 @@
+// HTML drag drop
+
+const boxArray = [
+  { title: ``, description: ``, status: ``, priority: `` },
+  { title: ``, description: ``, status: ``, priority: `` },
+  { title: ``, description: ``, status: ``, priority: `` },
+];
+
+boxArray.forEach(el) => {
+    boxString += `<div ondragstart="drag(event)" class="box" dragabble`
+}
--- 6-DOM/toDoList/script.js
+++ 6-DOM/toDoList/script.js
@@ -1,65 +1,93 @@
 const root = document.getElementById(`root`);
 
+// MAIN CONTAINER
 const mainContainer = document.createElement(`div`);
 mainContainer.setAttribute(`class`, `mainContainer`);
 root.appendChild(mainContainer);
 
+// HEADER CONTAINER
 const headerContainer = document.createElement(`div`);
 headerContainer.setAttribute(`class`, `headerContainer`);
 mainContainer.appendChild(headerContainer);
 
-const toDoContainer = document.createElement(`div`);
-toDoContainer.setAttribute(`class`, `toDoContainer`);
-toDoContainer.setAttribute(`id`, `toDoContainer`);
-mainContainer.appendChild(toDoContainer);
-
+// HEADER CONTAINER - H1
 const header = document.createElement(`h1`);
 header.setAttribute(`class`, `h1`);
 header.innerText = `To-Do List`;
 headerContainer.appendChild(header);
 
+// HEADER CONTAINER - INPUT TAG
 const input = document.createElement(`input`);
 input.setAttribute(`class`, `input`);
 input.setAttribute(`id`, `input`);
 input.placeholder = `Enter task here...`;
 headerContainer.appendChild(input);
 
+// HEADER CONTAINER - ADD TASK BUTTON
 const addBtn = document.createElement(`button`);
 addBtn.setAttribute(`id`, `addBtn`);
 addBtn.innerText = `Add Task`;
 headerContainer.appendChild(addBtn);
 
+// TODO CONTAINER
+const toDoContainer = document.createElement(`div`);
+toDoContainer.setAttribute(`class`, `toDoContainer`);
+toDoContainer.setAttribute(`id`, `toDoContainer`);
+mainContainer.appendChild(toDoContainer);
+
+let inputArr = [];
+let counter = 0;
+
+// ADD TASK FUNCTION
 addBtn.addEventListener(`click`, addTask);
 function addTask() {
-  let task = input.value;
-  //   console.log(`task :`, task);
+  inputArr[inputArr.length] = input.value;
+  counter = inputArr.length - 1;
+  // console.log({ counter });
 
+  // TASK GROUP DIV
   const taskGroup = document.createElement(`div`);
   taskGroup.setAttribute(`class`, `taskGroup`);
   taskGroup.setAttribute(`id`, `taskGroup`);
   toDoContainer.appendChild(taskGroup);
 
+  // TODO DIV, TODOTEXT P
   const toDo = document.createElement(`div`);
-  const toDoText = document.createElement(`p`);
-  toDoText.setAttribute(`class`, `toDoText`);
-  taskGroup.appendChild(toDo);
   toDo.setAttribute(`class`, `toDo`);
   toDo.setAttribute(`id`, `toDo`);
-  toDo.appendChild(toDoText);
+  taskGroup.appendChild(toDo);
 
-  toDoText.innerText = task;
-  //   console.log(`toDoText :`, toDoText);
+  toDo.innerText = inputArr[inputArr.length - 1];
 
   // DELETE BUTTON
   const dltBtn = document.createElement(`button`);
   dltBtn.setAttribute(`class`, `dltBtn`);
+  dltBtn.setAttribute(`id`, `dltBtn`);
   dltBtn.innerText = `Delete`;
   taskGroup.appendChild(dltBtn);
 
   dltBtn.addEventListener(`click`, deleteTask);
-}
 
-function deleteTask() {
-  const selDelTask = document.getElementById(`toDo`);
-  selDelTask.parentElement.remove();
+  // DELETE FUNCTION
+  function deleteTask() {
+    document.getElementById(`taskGroup`);
+    taskGroup.remove();
+  }
+
+  // EDIT BUTTON
+  const editBtn = document.createElement(`button`);
+  editBtn.setAttribute(`class`, `editBtn`);
+  editBtn.innerText = `Edit`;
+  taskGroup.appendChild(editBtn);
+
+  editBtn.addEventListener(`click`, editTask);
+
+  // EDIT FUNCTION
+  function editTask() {
+    const editToDo = document.createElement(`input`);
+    editToDo.setAttribute(`class`, `editToDo`);
+    editToDo.setAttribute(`id`, `editToDo`);
+    toDo.appendChild(editToDo);
+    editToDo.defaultValue = inputArr[counter];
+  }
 }
