2494fb0a06a9ba20ddac387bb8a57d13499f1c47
TemulenPinecone <pineconetemulen@gmail.com>
2024-01-04 15:50:50 +0800

    i7 class

--- 6-DOM/2medium/toDolist-2/script.js
+++ 6-DOM/2medium/toDolist-2/script.js
@@ -113,6 +113,7 @@ function drawBoard() {
   }
 }
 drawBoard();
+render();
 
 // MODAL CONTAINER
 let modalCont = document.createElement(`div`);
@@ -128,6 +129,7 @@ let modalTitle = document.createElement(`p`);
 modalTitle.innerText = `Title`;
 let titleInput = document.createElement(`input`);
 titleInput.setAttribute(`type`, `text`);
+titleInput.setAttribute(`id`, `titleInput`);
 modal.appendChild(modalTitle);
 modal.appendChild(titleInput);
 
@@ -135,6 +137,7 @@ let modalDesc = document.createElement(`p`);
 modalDesc.innerText = `Description`;
 let descInput = document.createElement(`input`);
 descInput.setAttribute(`type`, `text`);
+descInput.setAttribute(`id`, `descInput`);
 modal.appendChild(modalDesc);
 modal.appendChild(descInput);
 
@@ -142,6 +145,7 @@ let statusTxt = [`To do`, `In progress`, `Stuck`, `Done`];
 let modalStatus = document.createElement(`p`);
 modalStatus.innerText = `Status`;
 let statusSelect = document.createElement(`select`);
+statusSelect.setAttribute(`id`, `statusSelect`);
 modal.appendChild(modalStatus);
 modal.appendChild(statusSelect);
 for (let i = 0; i < statusTxt.length; i++) {
@@ -155,6 +159,7 @@ let priorityTxt = [`Low`, `Medium`, `High`];
 let modalPriority = document.createElement(`p`);
 modalPriority.innerText = `Priority`;
 let prioritySelect = document.createElement(`select`);
+prioritySelect.setAttribute(`id`, `prioritySelect`);
 modal.appendChild(modalPriority);
 modal.appendChild(prioritySelect);
 for (let i = 0; i < priorityTxt.length; i++) {
@@ -168,6 +173,44 @@ let submitBtn = document.createElement(`button`);
 submitBtn.setAttribute(`class`, `submitBtn`);
 submitBtn.innerText = `Submit`;
 modal.appendChild(submitBtn);
+
+function render() {
+  const cards1 = document.getElementById(`ToDoCards`);
+  const cards2 = document.getElementById(`InProgressCards`);
+  const cards3 = document.getElementById(`StuckCards`);
+  const cards4 = document.getElementById(`DoneCards`);
+  cards1.innerHTML = "";
+  cards2.innerHTML = "";
+  cards3.innerHTML = "";
+  cards4.innerHTML = "";
+
+  todos.map((a) => {
+    if (a.status == `To do`) {
+      cards1.appendChild(createCard(a));
+    } else if (a.status == `In progress`) {
+      cards2.appendChild(createCard(a));
+    } else if (a.status == `Stuck`) {
+      cards3.appendChild(createCard(a));
+    } else if (a.status == `Done`) {
+      cards4.appendChild(createCard(a));
+    }
+  });
+}
+
+submitBtn.onclick = function modalValue() {
+  let modalTitle = document.getElementById(`titleInput`).value;
+  let modalDesc = document.getElementById(`descInput`).value;
+  let modalStatus = document.getElementById(`statusSelect`).value;
+  let modalPriority = document.getElementById(`prioritySelect`).value;
+  todos.push({
+    title: modalTitle,
+    description: modalDesc,
+    status: modalStatus,
+    priority: modalPriority,
+  });
+  console.log(todos);
+  render();
+};
 
 // ADD CARD BUTTON SHOW  OR NOT SHOW
 let addBtnToDo = document.getElementById(`addBtnToDo`);
@@ -199,13 +242,13 @@ function createCard(x) {
   const cardDiv = document.createElement(`div`);
   const doneSign = document.createElement(`div`);
   const doneSignI = document.createElement(`i`);
-  doneSignI.innerText = `i = fas fa-check`;
+  doneSignI.innerText = `^^`;
   const detail = document.createElement(`div`);
   const detailH4 = document.createElement(`h4`);
   const detailP = document.createElement(`p`);
   const detailDiv = document.createElement(`div`);
-
   const actions = document.createElement(`div`);
+  actions.innerText = `Symbol`;
 
   cardDiv.setAttribute(`class`, `cardDiv`);
   doneSign.setAttribute(`class`, `doneSign`);
@@ -221,6 +264,7 @@ function createCard(x) {
   cardDiv.appendChild(doneSign);
   cardDiv.appendChild(detail);
   cardDiv.appendChild(actions);
+  doneSign.appendChild(doneSignI);
   detail.appendChild(detailH4);
   detail.appendChild(detailP);
   detail.appendChild(detailDiv);
@@ -228,18 +272,20 @@ function createCard(x) {
   return cardDiv;
 }
 
-todos.map((a) => {
-  if (a.status == `To do`) {
-    cards = document.getElementById(`ToDoCards`);
-    cards.appendChild(createCard(a));
-  } else if (a.status == `In progress`) {
-    cards = document.getElementById(`InProgressCards`);
-    cards.appendChild(createCard(a));
-  } else if (a.status == `Stuck`) {
-    cards = document.getElementById(`StuckCards`);
-    cards.appendChild(createCard(a));
-  } else if (a.status == `Done`) {
-    cards = document.getElementById(`DoneCards`);
-    cards.appendChild(createCard(a));
-  }
-});
+// todos.map((a) => {
+//   if (a.status == `To do`) {
+//     cards = document.getElementById(`ToDoCards`);
+//     cards.appendChild(createCard(a));
+//   } else if (a.status == `In progress`) {
+//     cards = document.getElementById(`InProgressCards`);
+//     cards.appendChild(createCard(a));
+//   } else if (a.status == `Stuck`) {
+//     cards = document.getElementById(`StuckCards`);
+//     cards.appendChild(createCard(a));
+//   } else if (a.status == `Done`) {
+//     cards = document.getElementById(`DoneCards`);
+//     cards.appendChild(createCard(a));
+//   }
+// });
+
+// input/typecheckbox
